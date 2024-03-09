4dca4d94ba4eb9bbd315d9542a8e4f48c75ad52e
AsadShibli <mdasadullahshibli@gmail.com>
2024-03-09 21:42:30 +0600

    commiting

--- main.py
+++ main.py
@@ -23,21 +23,23 @@ def findEncodings(images):
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
+        encodeList.append(face_recognition.face_encodings(img)[0])
     return encodeList
 
 
+markedAttendees = set()  # Initialize a set to keep track of marked attendees
+
 def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-        nameList.append(entry[0])
-        if name not in nameList:
+    global markedAttendees  # Access the global set variable
+    common_name = name.split('_')[0]  # Extract the common part of the name
+    if common_name not in markedAttendees:  # Check if the common name is already marked
+        with open('Attendance.csv', 'a') as f:  # Use 'a' mode to append to the file
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dtString}')
+            f.write(f'{common_name},{dtString}\n')  # Write the common name to the file
+        markedAttendees.add(common_name)  # Add the common name to the set
+
+
 
 
 
@@ -67,15 +69,17 @@ while True:
         print(faceDis)
         matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
+        if faceDis[matchIndex] < 0.50:
             name = classNames[matchIndex].upper()
-            # print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
             markAttendance(name)
+        else:
+            name = 'Unknown'
+        # print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
